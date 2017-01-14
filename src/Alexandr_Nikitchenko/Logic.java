b44b4a1e2670ce59b9c929bf9124d732b58e6113
Alexandr <photoluxe@mail.ru>
2017-01-14 22:13:21 +0600

    Температурный конвертер в графической оболочке

--- src/Alexandr_Nikitchenko/Logic.java
+++ src/Alexandr_Nikitchenko/Logic.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionListener;
 public class Logic implements ActionListener {
 
     GTCInterface parent;
-    double result = 0;
+    double result;
 
     Logic(GTCInterface parent) {
         this.parent = parent;
@@ -27,28 +27,27 @@ public class Logic implements ActionListener {
             String dispFieldText = parent.displayed.getText();
             double displayValue;
 
-            // Конвертация строки в число
-            displayValue = Double.parseDouble(dispFieldText);
+            // Получить число из дисплея + Конвертация строки в число
+                displayValue = Double.parseDouble(dispFieldText);
+
 
             // Создание объекта
             Object src = e.getSource();
 
             // Реакция на выбор метода и кнопки Конвертировать
             if (parent.r1.isSelected() && src == parent.buttonConvert ) {
-                String b;
-                b = "Ваша температура в Фаренгейтах равна: ";
-                parent.l3.setText(b);
+                String a = "Ваша температура в Фаренгейтах равна: ";
+                parent.l3.setText(a);
                 result = displayValue * 1.8 + 32;
-                String a = "" + result + "F";
-                parent.l4.setText(a);
+                String b = result + "F";
+                parent.l4.setText(b);
 
             } else if (parent.r2.isSelected() && src == parent.buttonConvert) {
-                String b;
-                b = "Ваша температура в Цельсиях равна: ";
-                parent.l3.setText(b);
+                String c = "Ваша температура в Цельсиях равна: ";
+                parent.l3.setText(c);
                 result = (displayValue - 32) * 5 / 9;
-                String a = "" + result + "C";
-                parent.l4.setText(a);
+                String d = result + "C";
+                parent.l4.setText(d);
             }
 
             // Реакция на нажатие кнопки Сброс
@@ -60,7 +59,6 @@ public class Logic implements ActionListener {
 
             // При возникновении ошибок обнулить дисплей
         }catch(Exception e1){
-            JOptionPane.showMessageDialog ( null, "Введите цифры..." );
             parent.displayed.setText(null);
         }
 
